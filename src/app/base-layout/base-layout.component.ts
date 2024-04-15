752d91eedb43b4448fc241e4fa9cc0483036f93e
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-04-15 08:36:52 +0530

    code for sidenav skeleton

--- src/app/base-layout/base-layout.component.ts
+++ src/app/base-layout/base-layout.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+// import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatDrawerMode, MatSidenavModule} from '@angular/material/sidenav';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-base-layout',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatButtonModule, MatRadioModule],
   templateUrl: './base-layout.component.html',
   styleUrl: './base-layout.component.scss'
 })
 export class BaseLayoutComponent {
-
+  mode :any= 'side';
+  shouldRun = true;
 }
