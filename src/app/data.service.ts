a4251ea6154856d7585f63c7c664263668cc2816
Manikantha-M <manikanta.iiitnuz@gmail.com>
2024-06-04 07:36:23 +0530

    data service to make http requests

---
+++ src/app/data.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'http://localhost:3000';
+
+  // Since Angular app and Node.js server are on the same host, you can use a relative URL '/api'
+
+  constructor(private http: HttpClient) { }
+
+
+  // GET request
+  get(endpoint: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${endpoint}`);
+  }
+
+  // POST request
+  post(endpoint: string, data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/${endpoint}`, data, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
+
+  // PATCH request
+  patch(endpoint: string, data: any): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${endpoint}`, data, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
+
+  // DELETE request
+  delete(endpoint: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${endpoint}`);
+  }
+  
+}
